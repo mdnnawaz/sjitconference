f2a85605ef90ab042074cc8213f1bbc496919dda
Mohammed Nawaz <nawazsherif97@gmail.com>
2017-06-24 12:13:43 +0530

    bug fix

--- app/controllers/journal_details_controller.rb
+++ app/controllers/journal_details_controller.rb
@@ -67,6 +67,7 @@ class JournalDetailsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_journal_detail
       @journal_detail = JournalDetail.find(params[:id])
+      
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/journal_detail.rb
+++ app/models/journal_detail.rb
@@ -1,6 +1,7 @@
 class JournalDetail < ApplicationRecord
   belongs_to :user
   has_attached_file :paper
+  
   do_not_validate_attachment_file_type :paper
 
 
