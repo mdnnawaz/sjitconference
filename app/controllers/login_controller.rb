a1b49054680a3e0e5dcfa99bf038fe8636f93519
Mohammed Nawaz <nawazsherif97@gmail.com>
2017-06-29 18:46:18 +0530

    login bug fix

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -11,17 +11,27 @@ class LoginController < ApplicationController
 			end
 			response = HTTParty.post("https://portal.stjosephstechnology.ac.in/Authenticate?user=#{@us}&pass=#{@pw}")
 			if response.include? "staff"
-			@user = User.find_by_username(params[:user])
-			if @user.nil?
-				@user=User.new(:username => @us, :password => @pw, :password_confirmation=> @pw)
-				@user.save
+				@user = User.find_by_username(params[:user])
+				if @user.nil?
+					@user=User.new(:username => @us, :password => @pw, :password_confirmation=> @pw)
+					# @user.id=User.id
+					@user.save
+
+					puts "user #{@user.id}"
+					# after_sign_up_path_for(@user)
+					sign_in(@user)
+					sign_in @user, :bypass => true 
+					redirect_to index_path
+					# after_sign_in_path_for(@user)
+				else
+					sign_in(@user)
+					sign_in @user, :bypass => true 
+					redirect_to index_path
+					# after_sign_in_path_for(@user)
+				end
+			else 
+				render html: "<strong>Authentication Failed</strong>".html_safe
 			end
-			sign_in(@user)
-			redirect_to index_path
-			# after_sign_in_path_for(@user)
-		else 
-			render html: "<strong>Authentication Failed</strong>".html_safe
-		end
 
 		end
 	end
